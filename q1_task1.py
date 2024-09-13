872c226af3086a69774ce452054468d1a690c763
jackie-trac <149240059+jackie-trac@users.noreply.github.com>
2024-09-14 08:57:16 +0930

    Update q1_task1.py
    Remove csv file paths for universal context

--- q1_task1.py
+++ q1_task1.py
@@ -1,23 +1,18 @@
 import pandas as pd
 
-# List of CSV file names and paths
-csv_files = [
-    'D:/Assignment2/CSV1.csv',
-    'D:/Assignment2/CSV2.csv',
-    'D:/Assignment2/CSV3.csv',
-    'D:/Assignment2/CSV4.csv'
-]
+# List of CSV file names, spcecify paths stored somewhere else
+csv_files = ['CSV1.csv','CSV2.csv','CSV3.csv','CSV4.csv']
 
 # Output file path
-output_file = 'all_values_text.txt'
+output_file = 'question1_task1_alltext.txt'
 
 # Open the output file in write mode
 with open(output_file, 'w') as f_out:
-    # Loop through each CSV file
+    # Iterate through each CSV file
     for file in csv_files:
-        df = pd.read_csv(file)  # Load CSV
+        df = pd.read_csv(file)  # Load CSV file to dataframe
         
-        # Write the header (column names)
+        # Write the header column names and move to the next line
         f_out.write(','.join(df.columns) + '\n')
         
         # Write each row in the dataframe
@@ -25,4 +20,4 @@ with open(output_file, 'w') as f_out:
             # Convert the row to a string and write it to the file
             f_out.write(','.join(row.astype(str)) + '\n')
 
-print(f"Data has been written successfully to {output_file}")
+print(f"All texts have been successfully extracted to {output_file}")
